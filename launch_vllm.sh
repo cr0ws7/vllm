8eea61e12697cc0e8e92a8b2d46fef9e70dc598f
cr0ws7 <44927507+cr0ws7@users.noreply.github.com>
2025-08-02 00:13:12 +0200

    use kv cache

--- launch_vllm.sh
+++ launch_vllm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# export VLLM_ATTENTION_BACKEND=FLASHINFER
+export VLLM_ATTENTION_BACKEND=FLASHINFER
 source .venv/bin/activate
 .venv/bin/vllm serve ~/ai/models/qwen3-14b-gptq \
   --served-model-name qwen3-14b-gptq \
   --trust-remote-code \
   --dtype auto \
   --gpu-memory-utilization 0.71 \
-  --max-model-len 2000 \
-  --max-num-batched-tokens 2000 \
+  --max-model-len 5000 \
+  --max-num-batched-tokens 5000 \
   --max-num-seqs 1 \
   --port 8000 \
   --enable-auto-tool-choice \
   --tool-call-parser hermes \
   --reasoning-parser deepseek_r1 \
-  # --kv-cache-dtype fp8 \
+  --kv-cache-dtype fp8 \
