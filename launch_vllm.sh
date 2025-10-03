0336158ac7bfab9b53fcaa6555b9ba550542848b
cr0ws7 <44927507+cr0ws7@users.noreply.github.com>
2025-10-03 14:49:35 +0200

    optimize settings

--- launch_vllm.sh
+++ launch_vllm.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export PYTORCH_CUDA_ALLOC_CONF="garbage_collection_threshold:0.6,max_split_size_mb:128,expandable_segments:True"
+export TORCH_CUDA_ARCH_LIST=sm_100   # reduce compile targets for your Blackwell GPU
 export VLLM_ATTENTION_BACKEND=FLASHINFER
 source .venv/bin/activate
 .venv/bin/vllm serve ~/ai/models/qwen3-14b-gptq \
   --served-model-name qwen3-14b-gptq \
   --trust-remote-code \
   --dtype auto \
-  --gpu-memory-utilization 0.71 \
-  --max-model-len 5000 \
-  --max-num-batched-tokens 5000 \
+  --gpu-memory-utilization 0.74 \
+  --max-model-len 8000 \
+  --max-num-batched-tokens 8000 \
   --max-num-seqs 1 \
   --port 8000 \
   --enable-auto-tool-choice \
   --tool-call-parser hermes \
   --reasoning-parser deepseek_r1 \
   --kv-cache-dtype fp8 \
+  --enforce-eager \
